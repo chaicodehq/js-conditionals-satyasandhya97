e36df9e662144a639c465492304efec7426d8563
satyasandhya97 <satyasandhyabiswal97@gmail.com>
2026-02-08 09:28:04 +0530

    complete the ticket-pricing program
    with '#' will be ignored, and an empty message aborts the commit.

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -24,4 +24,16 @@
  */
 export function getTicketPrice(age, isWeekend) {
   // Your code here
+  let price;
+
+  if (typeof age !== 'number' || age < 0) return -1;
+  else if (age <= 12) price = 8;
+  else if (age <= 13 && age <= 17) price = 12;
+  else if (age >= 13 && age <= 17) price = 12;
+  else if (age >= 18 && age <= 59) price = 15;
+  else price = 10;
+
+  if (isWeekend == true) price += 3;
+
+  return price;
 }
